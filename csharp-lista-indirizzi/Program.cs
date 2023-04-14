ba1643e25bbc15d879fd5cad8a5ec329b6b58a8c
Salvatore Esposito <esposito.00@outlook.it>
2023-04-14 23:28:11 +0200

    corrected

--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -1,6 +1,7 @@
 ﻿//In questo esercizio dovrete leggere un file CSV e salvare tutti gli indirizzi contenuti al sul interno all’interno di una lista di oggetti istanziati a partire dalla classe Indirizzo.
 //Attenzione: gli ultimi 3 indirizzi presentano dei possibili “casi particolari” che possono accadere a questo genere di file: vi chiedo di pensarci e di gestire come meglio crediate queste casistiche.
 
+
 using csharp_lista_indirizzi;
 
 List <Address> addresses = new List<Address> ();
@@ -12,7 +13,6 @@ void OpenFile()
         Console.WriteLine("ADDRESSES");
     
         //!textFile.EndOfStream  (finchè non finisce il testo del file)
-
         for (int i = 0; i < textFile.BaseStream.Length; i++ )
         { 
             string line = textFile.ReadLine();
@@ -48,8 +48,8 @@ void OpenFile()
                 Console.WriteLine($"{data[2]}, {data[3]}, {data[4]}, {data[5]}");
                 addresses.Add(new Address(data[2], data[3], data[4], data[5]));
             }
-            //7
-            else if (i == textFile.BaseStream.Length -1)
+            
+            else if (i == 7)
             {
                 string[] data = line.Split(',');
 
@@ -68,64 +68,11 @@ void OpenFile()
     {
         Console.WriteLine("Si è verificato un problema specifico: " + e.Message);
     }
+
     catch (Exception e)
     { 
         Console.WriteLine("Errore generico: " + e.Message);
     }
 }
 
-    OpenFile();
-
-
-
-
-
-
-
-
-
-
-
-
-//addresses.Add(new Address(""));
-
-
-//if (!File.Exists("dati\\utenti.txt"))
-//    Console.WriteLine("File non trovato!");
-//else
-//{
-//    try
-//    {
-//        StreamReader readFile = File.OpenText("dati\\utenti.txt");  //apro il file in lettura (nella stessa cartella dell'eseguibile)
-
-//        while (!readFile.EndOfStream)      //finché ci sono dati da leggere
-//        {
-//            string? line = readFile.ReadLine();   //leggo una riga dal file
-//            if (line != null)
-//            {
-//                string[] campi = line.Split(' ');   //suddividi la stringa in più pezzi usando lo spazio come separatore
-//                Console.WriteLine("UTENTE:");
-//                Console.WriteLine($"\tNome: {campi[0]}");
-//                Console.WriteLine($"\tCognome: {campi[1]}");
-//                Console.WriteLine($"\tData di nascita: {campi[2]}");
-//                addresses.Add(new Address(campi[0], campi[1], DateOnly.ParseExact(campi[2], "d")));
-//            }
-
-//        }
-//        readFile.Close();     //chiudo il file
-//    }
-//    catch (UnauthorizedAccessException)
-//    {
-//        Console.WriteLine("Non hai i diritti di accesso al file");
-//    }
-//    catch (Exception e)
-//    {
-
-//        Console.WriteLine("Si è verificato un problema: " + e.Message);
-//    }
-//}
-
-//StreamWriter sw = File.CreateText("output.txt");
-//sw.WriteLine("prova1");
-//sw.WriteLine("prova2");
-//sw.Close();
+OpenFile();
