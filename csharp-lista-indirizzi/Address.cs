74833e9842b90a6f959e1f2233a2b483a09a1543
Salvatore Esposito <esposito.00@outlook.it>
2023-04-14 15:50:01 +0200

    added Address class

--- csharp-lista-indirizzi/Address.cs
+++ csharp-lista-indirizzi/Address.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace csharp_lista_indirizzi
 {
-    internal class Address
+    public class Address
     {
+        public string Street { get; set; }
+        public string City {get; set;}
+        public string Province {get; set;}
+        public string Zip {get; set;}
 
+        public Address(string street, string city, string province, string zip)
+        {
+            Street = street;
+            City = city;
+            Province = province;
+            Zip = zip;
+        }
     }
 }
